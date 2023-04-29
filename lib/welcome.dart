17dc8f0bae084e5e5a3f226b860c4eb01ed0e75b
Bewave-27 <91317496+yvette0802@users.noreply.github.com>
2023-04-29 15:51:34 +0800

    Lobby/ Welcome Screen
    April 29, 2023
    3:51 pm

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -5,141 +5,151 @@ import 'package:project1/home_page.dart';
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final logo =
-        'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png';
     return Scaffold(
       body: Container(
-          color: Colors.black,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Opacity(
-                  opacity: 0.3,
-                  child: Image.asset('images/Welcome2.jpg', fit: BoxFit.cover),
-                ),
+        //color: Color.fromRGBO(255, 252, 185, 1),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/welcomeBg.png"), fit: BoxFit.cover),
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    child: ClipOval(
+                      child: Image.asset('images/Logo.png',
+                          width: 150, height: 150, fit: BoxFit.cover),
+                    ),
+                    radius: 90,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'SurviveIt',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Kids',
+                          style: TextStyle(
+                              color: Color.fromRGBO(249, 156, 35, 1),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                  SizedBox(height: 50),
+                  Container(
+                      margin: EdgeInsets.only(left: 70, right: 70, bottom: 5),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                splashColor: Color(0xFF80D8FF).withOpacity(0.5),
+                                highlightColor:
+                                    Color(0xFF80D8FF).withOpacity(0.3),
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MyHomePage()));
+                                },
+                                child: Container(
+                                    padding: EdgeInsets.all(15),
+                                    child: Text('Start Learning!',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 17,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold)),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(255, 138, 0, 1),
+                                    )),
+                              )))),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, bottom: 5),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AboutUsPage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 191, 116, 1),
+                        padding: EdgeInsets.all(13),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: Text(
+                        'Quiz',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, bottom: 5),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AboutUsPage()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 191, 116, 1),
+                        padding: EdgeInsets.all(13),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: Text(
+                        'About Us',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Stack(
+                  // children: <Widget>[
+                  //   Positioned.fill(
+                  //     child: Image.asset(
+                  //       "images/sceneryBottom.png",
+                  //       fit: BoxFit.fitWidth,
+                  //       alignment: Alignment.bottomCenter,
+                  //     ),
+                  //   ),
+                  // ]
+                  // ),
+                ],
               ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.transparent,
-                      child: ClipOval(
-                        child: Image.network(
-                          logo,
-                          width: 130,
-                          height: 130,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      radius: 110,
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'SurviveIt',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 80),
-                    Container(
-                        margin:
-                            EdgeInsets.only(left: 20, right: 20, bottom: 10),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Material(
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  splashColor:
-                                      Color(0xFF80D8FF).withOpacity(0.5),
-                                  highlightColor:
-                                      Color(0xFF80D8FF).withOpacity(0.3),
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                MyHomePage()));
-                                  },
-                                  child: Container(
-                                      padding: EdgeInsets.all(20),
-                                      child: Text('Start Learning!',
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold)),
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(50),
-                                          color: Colors.transparent,
-                                          border: Border.all(
-                                              color: Colors.white, width: 4))),
-                                )))),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AboutUsPage()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xFF303F9F),
-                          padding: EdgeInsets.symmetric(
-                              vertical: 30, horizontal: 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                        child: Text(
-                          'Quiz',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AboutUsPage()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Color(0xFF303F9F),
-                          padding: EdgeInsets.symmetric(
-                              vertical: 30, horizontal: 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                        ),
-                        child: Text(
-                          'About Us',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
